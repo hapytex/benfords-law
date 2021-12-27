a2c64e4be84c4f78ba4bd20dd04b60ae3d95696f
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-12-27 16:45:41 +0100

    initial version of the project

---
+++ src/Number/Benford.hs
@@ -0,0 +1,38 @@
+module Number.Benford (
+    firstDigit, firstDigit10
+  , firstDigit', firstDigit10'
+  , startSequence', startSequence10'
+  , startSequence10
+  ) where
+
+import Data.Foldable(foldl')
+
+firstDigit10 :: Floating a => Int -> Maybe a
+firstDigit10 = firstDigit 10
+
+firstDigit :: Floating a => Int -> Int -> Maybe a
+firstDigit radix digit
+  | digit < 1 || digit >= radix = Nothing
+  | otherwise = Just (firstDigit' radix digit)
+
+firstDigit10' :: Floating a => Int -> a
+firstDigit10' = firstDigit' 10
+
+firstDigit' :: Floating a => Int -> Int -> a
+firstDigit' radix digit = logBase (fromIntegral radix) ((d + 1) / d)
+    where d = fromIntegral digit
+
+startSequence10' :: Floating a => Int -> a
+startSequence10' n = log (1 + 1 / n') / log 10
+  where n' = fromIntegral n
+
+startSequence10 :: Floating a => Int -> Maybe a
+startSequence10 n
+  | n > 0 = Just (startSequence10' n)
+  | otherwise = Nothing
+
+startSequence' :: Floating a => Int -> [Int] -> a
+startSequence' radix ns = logBase (fromIntegral radix) (1 + 1 / fromIntegral d)
+  where d = foldl' go 0 ns
+          where go q = (r*q +) . (fromIntegral :: Int -> Integer)
+        r = fromIntegral radix
